035a129b295f9c64eef0134f5bfff6f57fde86db
fabionicolau <fabionicolaujr@gmail.com>
2022-02-18 16:09:44 -0300

    Requisitos 5 e 6

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import Planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        <div>
+          {Planets.map((eachPlanets, index) => (
+            <PlanetCard
+              key={ index }
+              planetName={ eachPlanets.name }
+              planetImage={ eachPlanets.image }
+            />
+          ))}
+        </div>
       </div>
     );
   }
